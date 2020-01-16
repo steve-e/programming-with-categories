10bc247a966385d2f58c260f5bc6f27473e6af96
Steve Etherington <steve.etheringtonovoenergy.com>
2020-01-16 16:08:29 +0000

    some more of the exercises suggested in Lecture 5

--- src/Lecture6.hs
+++ src/Lecture6.hs
@@ -8,3 +8,9 @@ toFst = tuple (id, const ())
 toSnd :: b -> ((), b)
 toSnd = tuple (const (), id)
 
+commuteL :: (a1, (a2, a3)) -> ((a1, a2), a3)
+commuteL = tuple (tuple (fst ,fst.snd ), snd.snd )
+
+commuteR :: ((a1, a2), a3) -> (a1, (a2, a3))
+commuteR = tuple (fst.fst, tuple (snd.fst, snd))
+
